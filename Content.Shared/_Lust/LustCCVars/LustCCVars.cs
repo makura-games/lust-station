17a367eb81510ec836dda323d77836b130a3216e
DeuMatizzze <kekelen230@gmail.com>
2026-09-21 09:02:21 +0700

    Добавление LustCCVars

---
+++ Content.Shared/_Lust/LustCCVars/LustCCVars.cs
@@ -0,0 +1,31 @@
+﻿using Content.Shared.Clothing.Components;
+using Robust.Shared;
+using Robust.Shared.Configuration;
+
+namespace Content.Shared._Lust.LustCCVars;
+
+[CVarDefs]
+public sealed partial class LustCCVars : CVars
+{
+
+    #region Alternative VoteManager Alternation
+
+    public static readonly CVarDef<bool> LustAltVotingEnabled =
+        CVarDef.Create("lust.alt_voting.enabled", true);
+
+    public static readonly CVarDef<bool> LustPeacefulAlternation =
+        CVarDef.Create("lust.alt_vote.peaceful_alternation", true);
+
+    public static readonly CVarDef<string> LustAltVoteMainPreset =
+        CVarDef.Create("lust.alt_vote.main_preset",
+            "LustPresetPoolAltDefault",
+            CVar.SERVERONLY);
+
+    public static readonly CVarDef<string> LustAltVotePeacefulPreset =
+        CVarDef.Create("lust.alt_vote.peaceful_preset",
+            "LustPresetPeacefulOnly",
+            CVar.SERVERONLY);
+
+    #endregion
+
+}
